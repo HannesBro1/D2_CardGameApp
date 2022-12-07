5706e6bb746a638205920ff08a40dc631840ecba
HannesBro1 <48271371+HannesBro1@users.noreply.github.com>
2022-12-07 16:08:12 +0100

    Logging and packages

--- src/main/java/msc/ddb/international/App.java
+++ src/main/java/msc/ddb/international/App.java
@@ -1,5 +1,11 @@
 package msc.ddb.international;
 
+import static java.time.Instant.now;
+import static java.time.temporal.ChronoUnit.MILLIS;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
 import java.util.Scanner;
 
 /**
@@ -13,13 +19,31 @@ public class App
     /** 
      * @param args
      */
+
+     //Starten vom Logger JUL
+    private static final Logger log = Logger.getLogger (App.class.getName());
+    
     public static void main( String[] args )
     {
+
+        // Maschinenzeit
+        Instant start = now();
+
+        // Datum+Zeit aus der lokalen Zeitzone
+        LocalDateTime localDateTime = LocalDateTime.now();
+        
+        System.out.println("\n");
+
+        log.info ("Starten Logging mit 'JUL' (Java Util Logging)");
         // create a game
         Game game = new Game();
+
+        // instance a player
         Player player01 = new Player("Harald");
         game.setPlayer(player01);
         game.startGame();
+        
+        
         // lets play!
         System.out.println("--------------------\nWelcome to BlackJack!\n\n");
         int decision = 1;
@@ -47,5 +71,9 @@ public class App
         }
         // how did it run
         System.out.println(game);
+
+        // Programmende:
+        System.out.println("\n");
+        log.info( () -> String.format( "Laufzeit %s ms", start.until( now(), MILLIS ) ) );
     }
 }
